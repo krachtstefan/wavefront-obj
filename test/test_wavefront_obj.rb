61b0db7859e742a5014cb50e0062b5339a73ecc2
Stefan Kracht <stefankracht@gmail.com>
2013-02-24 18:48:39 +0100

    added test for point adding

--- test/test_wavefront_obj.rb
+++ test/test_wavefront_obj.rb
@@ -10,5 +10,37 @@ describe WavefrontObj do
     @wavefront_obj.name = "Test"
     @wavefront_obj.name.must_equal "Test"
   end
+  
+  describe "when adding a point" do
+    before do
+      @wavefront_obj.add_point [1, 2, 3]
+    end
+    
+    it "has to be in the points array" do
+      @wavefront_obj.points.length.must_equal 1
+    end
+    
+    it "has to incremebt the point index" do
+      @wavefront_obj.point_index.must_equal 1
+    end
+      
+    describe "when adding the same point again" do
+      before do
+        @wavefront_obj.add_point [1, 2, 3]
+      end
+
+      it "still has to be a point index 1" do
+        @wavefront_obj.point_index.must_equal 1
+      end
+      
+      it "still has to have one point" do
+        @wavefront_obj.points.length.must_equal 1
+      end
+    end
+      
+    
+    
+    # check keys
+  end
 
 end
