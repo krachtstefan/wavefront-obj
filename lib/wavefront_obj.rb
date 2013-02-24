e2bfe2db8c630089207e550dc5d45de53b5a318c
Stefan Kracht <stefankracht@gmail.com>
2013-02-24 12:51:46 +0100

    create initializer

--- lib/wavefront_obj.rb
+++ lib/wavefront_obj.rb
@@ -2,5 +2,17 @@ require 'rubygems'
 require 'digest/md5'
 
 class WavefrontObj
+
+  attr_reader :point_index
+  attr_accessor :name, :points, :faces, :verbose
   
+  DEFAULT_NAME = "Wavefront Obj"
+  
+	def initialize
+		@verbose = false
+		@name = DEFAULT_NAME
+		@points = Hash.new
+		@point_index = 0
+		@faces = Hash.new
+	end
 end
