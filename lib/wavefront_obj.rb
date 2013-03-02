db1ea463957234416d37c941f6c07ccc606fc368
Stefan Kracht <stefankracht@gmail.com>
2013-03-02 11:35:51 +0100

    added yard documentation

--- lib/wavefront_obj.rb
+++ lib/wavefront_obj.rb
@@ -1,6 +1,7 @@
 require 'rubygems'
 require 'digest/md5'
 
+# @author Stefan Kracht
 class WavefrontObj
 
   attr_reader :point_index
@@ -8,13 +9,17 @@ class WavefrontObj
   
   DEFAULT_NAME = "Wavefront Obj"
   
+  # inits attributes
 	def initialize
 		@name = DEFAULT_NAME
 		@points = Hash.new
 		@point_index = 0
 		@faces = Hash.new
 	end
-  
+
+  # adds a point to the 3d object 
+  # @param pnt [Array] array of 3 coordinates
+  # @return [String, false] the point index or false
 	def add_point(pnt)
 		if pnt.class == Array && pnt.length == 3
 			indentifier = create_point_identifier(pnt)
@@ -30,7 +35,11 @@ class WavefrontObj
 		end
 	end
   
+  # adds a face to the 3d object 
+  # @param pnt_arr [Array] array of multiple point arrays
+  # @void
 	def add_face(pnt_arr)
+    # TODO add error handling
 		face_points = []
 		pnt_arr.each do |pnt|
 			face_points.push add_point(pnt)
@@ -42,13 +51,19 @@ class WavefrontObj
 		end
 	end
   
+  # stores the obj data as a file
+  # @param path [String] path to store the file, ".obj" will be added if missing
+  # @return [String] the path of the created file
 	def save(path)
+    # TODO add error handling
 		raw_data = get_raw_data
 		path = "#{path}.obj" unless path.split(".").last == "obj"
 		File.open(path, 'w') {|f| f.write(get_raw_data) }
 		path
   end
   
+  # returns the raw data of the obj file
+  # @return [String] raw data
 	def get_raw_data
 		buffer = []
 		buffer.push(get_name_syntax(@name))
@@ -66,23 +81,39 @@ class WavefrontObj
   
   private
   
+  # returns the syntax of object definition
+  # @param name [String] object name
+  # @return [String] object name in .obj format
   def get_name_syntax(name)
   	"o #{name}"
   end
 
+  # returns the syntax of a point definition
+  # @param point [String] object name
+  # @return [String, false] point in .obj format
   def get_point_syntax(point)
+    # TODO convert to array
   	"v #{point.join(" ")}"
   end
 
+  # returns the syntax of a face definition
+  # @return [String, false] face in .obj format
   def get_face_syntax(face)
+    # TODO convert to array
   	"f #{face.join(" ")}"
   end
-
+  
+  # generates the unique id of a point
+  # @return [String] point indetifier
   def create_point_identifier(pnt)
+    # TODO convert to array
     Digest::MD5.hexdigest(pnt.join("|"))
   end
-  
+
+  # generates the unique id of a face
+  # @return [String] face indetifier  
   def create_face_identifier(pnts)
+    # TODO convert to array
     Digest::MD5.hexdigest(pnts.join("|"))
   end
 
